311788941b1f4b5b0448c35f3d6f8b2342b30f40
Gabriel Moraes <gabrieltrabalho96@hotmail.com>
2024-10-30 21:44:00 -0300

    arquivos home.html e head.html criados e configurados

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def home(request):
-    return render(request,'home.html')
+    return render(request, 'pages/home.html', context={'name':'Gabriel',})
 
-def contato(request):
-    return HttpResponse('contato')
-
-def sobre(request):
-    return HttpResponse('sobre')
