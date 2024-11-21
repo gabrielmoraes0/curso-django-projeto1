c7bd00e37fe616e4fcaad4c4135e39689c617ad7
Gabriel Moraes <gabrieltrabalho96@hotmail.com>
2024-11-20 22:47:02 -0300

    criando arquivo de teste, e incrementando nos cards para teste

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-
+from projeto.utils.recipes.factory import make_recipe
 
 def home(request):
-    return render(request, 'pages/home.html', context={'name':'Gabriel',})
+    return render(request, 'pages/home.html', context={'recipes':[make_recipe() for _ in range(10)],})
 
+
+def recipe(request, id):
+    return render(request, 'pages/recipe-view.html', context={'recipe': make_recipe(),})
